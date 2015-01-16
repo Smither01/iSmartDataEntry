c0d93e06bdf6cf22b0b5927d312ca9ba6e74c258
Smither <Smither@192.168.113.103>
2015-01-16 17:38:51 +0800

    修正BUG

--- src/com/guogu/ismartdataentry/MainActivity.java
+++ src/com/guogu/ismartdataentry/MainActivity.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -40,6 +41,7 @@ public class MainActivity extends Activity implements OnClickListener,OnItemClic
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE); 
 		setContentView(R.layout.activity_main);
 		listJson = new ArrayList<JSONObject>();
 		objAdd = new JSONObject();
