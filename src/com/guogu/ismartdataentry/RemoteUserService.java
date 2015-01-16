21780b7e975ea76a2df1f563e20f68786fc4d11c
Smither <Smither@192.168.113.103>
2015-01-16 20:44:43 +0800

    修改到发布服务器

--- src/com/guogu/ismartdataentry/RemoteUserService.java
+++ src/com/guogu/ismartdataentry/RemoteUserService.java
@@ -6,9 +6,11 @@ import java.util.Map;
 import org.json.JSONArray;
 
 public class RemoteUserService {	
-	
+
+	public static final String DATA_SERVER_URL = "http://regsrv1.guogee.com:86/";
 	public static void sendData(String pkgID,String pkgPWD,JSONArray jsonArr, AsyncHttpResponseHandler handler){
-		String url = "http://115.28.165.93:86/" + "api/user/inputPackageDevices";
+		
+		String url = DATA_SERVER_URL + "api/user/inputPackageDevices";
 		Map<String,String> params = new HashMap<String,String>();
 		params.put("pkgID", pkgID);
 		params.put("pkgPWD", pkgPWD);
